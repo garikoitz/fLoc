1df6bf1532767ecd3c8543bc0c1f4a5f7ba1a96e
Tiger Yongning Lei <48440236+YongningLei@users.noreply.github.com>
2024-04-25 10:28:28 +0200

    create a function to transfer fLoc logs to onset.xls

---
+++ floc_add_path.m
@@ -0,0 +1,14 @@
+
+% add folder and subfolder of fLoc to path
+addpath(genpath('/Users/tiger/toolboxes/fLoc'))
+fprintf("fLoc and its subfolders have been add to the path \n")
+
+% addpath('/Users/tiger/toolboxes')
+% fprintf("toolbox folder has been add to the path \n")
+
+% add folder and subfolder of BCBLViennaSoft to path
+%addpath(genpath('/Users/tiger/Packages/toolbox/BCBLViennaSoft'))
+%fprintf("BCBLViennaSoft and its subfolder has been add to the path \n")
+% add folder and subfolder of Psychtoolbox-3 to path
+%addpath(genpath('/Users/tiger/Packages/toolbox/Psychtoolbox-3'))
+%fprintf("Psychtoolbox and its subfolder has been add to the path \n")
